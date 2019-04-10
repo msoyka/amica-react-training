16865e804e602d7c9ad455b509fa28be711fa967
“msoyka” <msoyka91@gmail.com>
2019-04-10 15:08:53 -0400

    (mod-1) Created SmartAvatar to fetch Github Avatar

--- src/components/Avatar/Avatar.stories.jsx
+++ src/components/Avatar/Avatar.stories.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import Avatar from "./index";
+import SmartAvatar from "./SmartAvatar";
 
-storiesOf("Avatar", module).add("default", () => <Avatar />);
+storiesOf("Avatar", module)
+  .add("default", () => <Avatar />)
+  .add("Smart Avatar", () => <SmartAvatar githubID="16545161" />);
