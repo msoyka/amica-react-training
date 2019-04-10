c74ebf5dfdae911d547a5643d13ea2c634cc81ef
“msoyka” <msoyka91@gmail.com>
2019-04-10 15:16:02 -0400

    (mod-1) Setup SmartAvatar exercise for mod-1-3.

--- src/components/Avatar/SmartAvatar.jsx
+++ src/components/Avatar/SmartAvatar.jsx
@@ -1,35 +1,9 @@
-import React from "react";
-import Avatar from "./index";
-import fetchGithubAvatar from "./fetchGithubAvatar";
+// import React from "react";
+// import Avatar from "./index";
+// import fetchGithubAvatar from "./fetchGithubAvatar";
 
-class SmartAvatar extends React.Component {
-  // Note: No longer needed to create constructor
-  // Read More: https://hackernoon.com/the-constructor-is-dead-long-live-the-constructor-c10871bea599
+// 🎯 To Do: Create a component that fetches a GitHub avatar image when mounted
+// ✅ Requirement: Show default image while GitHub avatar loads
+// ✅ Requirement: Switch default image with avatar image after fetched
 
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { avatar: {} };
-  // }
-
-  state = { avatar: {} };
-
-  async componentDidMount() {
-    try {
-      const response = await fetchGithubAvatar(this.props.githubID);
-      this.setState({ avatar: response });
-    } catch (error) {
-      console.error("error: ", error);
-    }
-  }
-
-  render() {
-    const { avatar } = this.state;
-    if (Object.keys(avatar).length) {
-      return <Avatar src={avatar.src} alt={avatar.alt} />;
-    } else {
-      return <Avatar />;
-    }
-  }
-}
-
-export default SmartAvatar;
+// 💰TIP: use fetchGithubAvatar.js
