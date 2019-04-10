874cee14ab387ae4816149a9dc9b0e742051ec9e
“msoyka” <msoyka91@gmail.com>
2019-04-10 13:43:51 -0400

    (mod-1.2) Style Avatar component with styled-components.

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,35 +1,15 @@
 import React from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
 import icon from "./avatar.svg";
 
-const Wrapper = styled.figure`
-  display: inline-block;
-  padding: 10px;
-  border-radius: 5px;
-  background-color: rgba(34, 204, 216, 0.7);
-  transition: all 0.2s ease-in-out;
+// To Do
+// 🎯 Use styled-components to styled Avatar component
 
-  &:hover {
-    background-color: rgba(34, 204, 216, 1);
-    transform: scale(1.1);
-  }
-`;
-
-// ES5 Function
-// function Avatar({ src = icon, alt = "Avatar Icon" }) {
-//   return (
-//     <Wrapper>
-//       <img src={src} alt={alt} />
-//     </Wrapper>
-//   );
-// }
-
-// ES6 Arrow Function
 const Avatar = ({ src = icon, alt = "Avatar Icon" }) => {
   return (
-    <Wrapper>
+    <figure>
       <img src={src} alt={alt} />
-    </Wrapper>
+    </figure>
   );
 };
 
