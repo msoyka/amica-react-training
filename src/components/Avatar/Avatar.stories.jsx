4da375d8e20e8cf58a5718c4a16838acae02aeaf
“msoyka” <msoyka91@gmail.com>
2019-04-10 12:01:18 -0400

    Setup mod-1 exercise

--- src/components/Avatar/Avatar.stories.jsx
+++ src/components/Avatar/Avatar.stories.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import Avatar from "./index";
-import avatarIcon from "./avatar.svg";
 
 storiesOf("Avatar", module).add("default", () => (
-  <Avatar src={avatarIcon} alt="Avatar Icon" />
+  <h1>Add Avatar component here.</h1>
 ));
--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,23 +1,2 @@
-import React from "react";
-
-// ES5 Function
-// function Avatar(props) {
-//   const { src, alt } = props;
-//   return (
-//     <div>
-//       <img src={src} alt={alt} />
-//     </div>
-//   );
-// }
-
-// ES6 Arrow Function
-const Avatar = props => {
-  const { src, alt } = props;
-  return (
-    <div>
-      <img src={src} alt={alt} />
-    </div>
-  );
-};
-
-export default Avatar;
+// To Do
+// 🎯 Create a React Avatar component that utilizes the avatar.svg
