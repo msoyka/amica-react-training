d4e170516514f4abf227bb1c3288c86eaa7784e6
“msoyka” <msoyka91@gmail.com>
2019-04-10 12:17:19 -0400

    (mod-1) Added styled-components to Avatar component.

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,21 +1,35 @@
 import React from "react";
+import styled from "styled-components";
 import icon from "./avatar.svg";
+
+const Wrapper = styled.figure`
+  display: inline-block;
+  padding: 10px;
+  border-radius: 5px;
+  background-color: rgba(34, 204, 216, 0.7);
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    background-color: rgba(34, 204, 216, 1);
+    transform: scale(1.1);
+  }
+`;
 
 // ES5 Function
 // function Avatar({ src = icon, alt = "Avatar Icon" }) {
 //   return (
-//     <div>
+//     <Wrapper>
 //       <img src={src} alt={alt} />
-//     </div>
+//     </Wrapper>
 //   );
 // }
 
 // ES6 Arrow Function
 const Avatar = ({ src = icon, alt = "Avatar Icon" }) => {
   return (
-    <div>
+    <Wrapper>
       <img src={src} alt={alt} />
-    </div>
+    </Wrapper>
   );
 };
 
