5d9f56267504dd3e9c1dbb6b3a6111f93544e9b3
“msoyka” <msoyka91@gmail.com>
2019-04-11 09:42:12 -0400

    Added createElement Avatar component

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import icon from "./avatar.svg";
 
+// React.createElement()
+// function Avatar({ src = icon, alt = "Avatar Icon" }) {
+//   return React.createElement(
+//     "div",
+//     null,
+//     React.createElement("img", { src, alt }, null)
+//   );
+// }
+
 // ES5 Function
 // function Avatar({ src = icon, alt = "Avatar Icon" }) {
 //   return (
