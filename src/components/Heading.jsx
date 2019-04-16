047695be6e898842140cf100f569a1df622b8b55
“msoyka” <msoyka91@gmail.com>
2019-04-16 11:30:52 -0500

    (mod 2-2) Transform heading functional component into styled-component wrapper

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,13 +1,10 @@
-import * as React from "react";
 import styled from "styled-components";
 
-const H1 = styled.h1`
+const Heading = styled.h1`
   font-family: Avenir, Arial, sans-serif;
   font-size: 2.625rem;
   font-weight: 600;
   line-height: 1.19;
 `;
-
-const Heading = ({ children }) => <H1>{children}</H1>;
 
 export default Heading;
