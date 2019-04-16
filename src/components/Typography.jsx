ff42a7467f99bdcd294f12bd0236a7bee6f3e716
“msoyka” <msoyka91@gmail.com>
2019-04-16 12:19:32 -0500

    (mod 2-4) Abstract common heading styles.

--- src/components/Typography.jsx
+++ src/components/Typography.jsx
@@ -1,14 +1,18 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+const headingStyles = css`
+  font-family: Avenir, Arial, sans-serif;
+`;
 
 export const H1 = styled.h1`
-  font-family: Avenir, Arial, sans-serif;
+  ${headingStyles}
   font-size: 2.625rem;
   font-weight: 600;
   line-height: 1.19;
 `;
 
-export const SubH1 = styled.h1`
-  font-family: Avenir, Arial, sans-serif;
+export const SubH1 = styled.span`
+  ${headingStyles}
   font-size: 1.5rem;
   line-height: 1.5;
   margin-bottom: 1.5rem;
