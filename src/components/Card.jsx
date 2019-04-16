f84e31a5069eb218268ed4c37234224d06339446
“msoyka” <msoyka91@gmail.com>
2019-04-16 16:10:00 -0500

    (mod 2-5) Setup Card component homework exercise

---
+++ src/components/Card.jsx
@@ -0,0 +1,8 @@
+// 🎯INSTRUCTIONS => './_designs/Card/card-specs.md'
+
+import React from "react";
+// import styled from "styled-components";
+
+const Card = () => <div>This is a card</div>;
+
+export default Card;
---
+++ src/components/card.stories.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Card from "./Card";
+// import { H1, SubH1 } from "./Typography";
+// import Button from "./Button";
+
+storiesOf("Card", module).add("Default", () => <Card />);
