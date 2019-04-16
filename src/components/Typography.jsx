2ecdb8beecc8896d3c52a3496f6159eb5f823c75
“msoyka” <msoyka91@gmail.com>
2019-04-16 11:50:02 -0500

    (mod 2-3) Create multiple exports for heading, subheading

---
+++ src/components/Typography.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const H1 = styled.h1`
+  font-family: Avenir, Arial, sans-serif;
+  font-size: 2.625rem;
+  font-weight: 600;
+  line-height: 1.19;
+`;
+
+export const SubH1 = styled.h1`
+  font-family: Avenir, Arial, sans-serif;
+  font-size: 1.5rem;
+  line-height: 1.5;
+  margin-bottom: 1.5rem;
+`;
