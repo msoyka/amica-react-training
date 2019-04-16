27fb2d59209048a041c24e75082c707af81598d9
“msoyka” <msoyka91@gmail.com>
2019-04-16 11:50:02 -0500

    (mod 2-3) Create multiple exports for heading, subheading

--- src/components/heading.stories.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import Heading from "./Heading";
-
-storiesOf("Heading", module).add("H1", () => (
-  <Heading>This is a Heading 1</Heading>
-));
