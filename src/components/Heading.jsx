9bb7b3acf4633593b049bc048f3589db8671700e
“msoyka” <msoyka91@gmail.com>
2019-04-16 11:35:27 -0500

    (mod 2-2) Setup exercise to Transform heading functional component into styled-component wrapper

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,10 +1,19 @@
+// 🎯To Do: Consolidate this component to just use the styled-component
+//        rather than using the styled-components within the React
+//        component that it is now.
+
+// 💰TIP: Do you actually need the React component?
+
+import * as React from "react";
 import styled from "styled-components";
 
-const Heading = styled.h1`
+const H1 = styled.h1`
   font-family: Avenir, Arial, sans-serif;
   font-size: 2.625rem;
   font-weight: 600;
   line-height: 1.19;
 `;
+
+const Heading = ({ children }) => <H1>{children}</H1>;
 
 export default Heading;
