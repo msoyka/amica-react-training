c41efa12cc88799a539e9c1db0e0c50a4e9e4655
“msoyka” <msoyka91@gmail.com>
2019-04-17 13:58:27 -0500

    (mod 2-5) Solution for adding props to styled-components.

--- src/components/typography.stories.jsx
+++ src/components/typography.stories.jsx
@@ -6,7 +6,28 @@ import { storiesOf } from "@storybook/react";
 import { H1, SubH1 } from "./Typography";
 
 storiesOf("Typography", module)
-  .add("Heading 1", () => <H1 propName="prop here">This is a Heading 1</H1>)
+  .add("Heading", () => (
+    <>
+      <H1>This is a default Heading 1</H1>
+      <H1 primary>This is a Primary Heading 1</H1>
+      <H1 primary={true}>This is a Primary Heading with boolean</H1>
+      <H1 primary={true} fontWeight={600}>
+        This is a Primary Heading with boolean and font weight 600
+      </H1>
+    </>
+  ))
   .add("SubHeading", () => (
-    <SubH1 propName="prop here">This is a SubHeading 1</SubH1>
+    <>
+      <SubH1>This is a Default SubHeading</SubH1>
+      <br />
+      <SubH1 borderColor="purple">This is an underlined SubHeading 1</SubH1>
+      <br />
+      <SubH1 shadow={true}>This is a SubHeading 1 with text shadow</SubH1>
+      <br />
+      <SubH1 spacing="5px">This is a SubHeading 1 with text spacing</SubH1>
+      <br />
+      <SubH1 borderColor="pink" shadow={true} spacing="5px">
+        Crazy Subheading
+      </SubH1>
+    </>
   ));
