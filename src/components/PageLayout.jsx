08c9baf8f9b8bd026070d97df6d7e504a9594bd3
“msoyka” <msoyka91@gmail.com>
2019-04-19 16:42:08 -0500

    (mod 3-4) Solution for using styled-components to create PageLayout component

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,52 +1,17 @@
-import React from "react";
 import styled from "styled-components";
-import { H1, SubH1 } from "./Typography";
 
-const Wrapper = styled.section`
+export const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
 `;
 
-const Header = styled.header`
+export const Header = styled.header`
   padding: 10px 5px;
   background-color: #efefef;
   color: gray;
 `;
 
-const Body = styled.div`
+export const Body = styled.div`
   padding: 1rem;
   width: 100%;
 `;
-
-const NoPropsComponent = () => (
-  <div>
-    <H1>No props passed to component</H1>
-    <SubH1>Please pass props for this component to function properly.</SubH1>
-  </div>
-);
-
-const DefaultHeader = () => <H1>Welcome to Amica</H1>;
-
-const DefaultBody = () => (
-  <SubH1>
-    Amica Mutual Insurance Company is a Rhode Island-based mutual insurance
-    company that offers auto, home and life insurance. Amica was founded in 1907
-    by A.T. Vigneron and originally offered auto, fire and theft insurance.
-  </SubH1>
-);
-
-const PageLayout = ({ header, body }) =>
-  header || body ? (
-    <Wrapper>
-      {(header && <Header>{header}</Header>) || <DefaultHeader />}
-      {(body && (
-        <Body>
-          <SubH1>{body()}</SubH1>
-        </Body>
-      )) || <DefaultBody />}
-    </Wrapper>
-  ) : (
-    <NoPropsComponent />
-  );
-
-export default PageLayout;
--- src/components/PageLayout.stories.jsx
+++ src/components/PageLayout.stories.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import PageLayout from "./PageLayout";
+import { Wrapper, Header, Body } from "./PageLayout";
 import { H1, SubH1 } from "./Typography";
 
-const Title = ({ name }) => <H1>Welcome to Amica, {name}</H1>;
+const name = "Matt";
 
-const Body = () => (
-  <SubH1>
-    Amica Mutual Insurance Company is a Rhode Island-based mutual insurance
-    company that offers auto, home and life insurance. Amica was founded in 1907
-    by A.T. Vigneron and originally offered auto, fire and theft insurance.
-  </SubH1>
-);
-
-storiesOf("Page Layout", module)
-  .add("No Header or Body", () => <PageLayout />)
-  .add("With Header", () => <PageLayout header={<Title name="Matt" />} />)
-  .add("With Body", () => <PageLayout body={Body} />)
-  .add("With Header & Body", () => (
-    <PageLayout header={<Title name="Matt" />} body={Body} />
-  ));
+storiesOf("Page Layout", module).add("Default", () => (
+  <Wrapper>
+    <Header>
+      <H1>Welcome to Amica, {name}</H1>
+    </Header>
+    <Body>
+      <SubH1>
+        Amica Mutual Insurance Company is a Rhode Island-based mutual insurance
+        company that offers auto, home and life insurance. Amica was founded in
+        1907 by A.T. Vigneron and originally offered auto, fire and theft
+        insurance.
+      </SubH1>
+    </Body>
+  </Wrapper>
+));
