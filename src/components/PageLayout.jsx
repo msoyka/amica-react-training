6473d84e712302ea8d443b6cccff444e3fbd405e
“msoyka” <msoyka91@gmail.com>
2019-04-19 14:27:53 -0500

    (mod 3-1) Setup PageLayout conditionals practice

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,3 +1,21 @@
+// ==========================================================================================================
+// 💥TO DO: Utilize as many of the conditional syntax examples below in
+//          order make the PageLayout less prone to runtime errors.
+//
+// 🗒 POPULAR CONDITIONAL SYNTAX EXAMPLES
+// ✅ if/else: if (header) {} else {}
+// ✅ ternary: header ? : ;
+// ✅ Logical AND Operator: true && 'Dog'
+// ✅ Logical OR Operator: false || 'Cat'
+// ✅ Double NOT (!!): !!true
+//
+// 💻 RESOURCES
+// 🔗 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_Operators
+//
+// 💰TIP: Styling has been done for you!
+// 💰TIP: Use the default components for graceful degradation.
+// ==========================================================================================================
+
 import React from "react";
 import styled from "styled-components";
 
@@ -14,12 +32,26 @@ const Header = styled.header`
 
 const Body = styled.div`
   padding: 1rem;
-  max-width: 800px;
-  margin: auto;
   width: 100%;
 `;
 
-const PageLayout = () => (
+const NoPropsComponent = () => (
+  <div>
+    <h1>No props passed to component</h1>
+  </div>
+);
+
+const DefaultHeader = () => <h6>Welcome to Amica</h6>;
+
+const DefaultBody = () => (
+  <p>
+    Amica Mutual Insurance Company is a Rhode Island-based mutual insurance
+    company that offers auto, home and life insurance. Amica was founded in 1907
+    by A.T. Vigneron and originally offered auto, fire and theft insurance.
+  </p>
+);
+
+const PageLayout = ({ header, body }) => (
   <Wrapper>
     <Header>Page Header</Header>
     <Body>Page Body Content</Body>
--- src/components/PageLayout.stories.jsx
+++ src/components/PageLayout.stories.jsx
@@ -1,8 +1,15 @@
-// 💥TO DO: Add own custom props where "propName" is currently located.
-// You can add more than one prop if you want.
+// ====================================================================================
+// 💥TO DO: Add the various conditional states of the PageLayout
+//          component that you created inside PageLayout.jsx
 //
+// 💰TIP: Add these stories below to Page Layout.
+//  .add("With Header", () => <PageLayout header={true} />)
+//  .add("With Body", () => <PageLayout body={true} />)
+//  .add("With Header & Body", () => <PageLayout header={true} body={true} />)
+// ====================================================================================
+
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import PageLayout from "./PageLayout";
 
-storiesOf("Page Layout", module).add("Default", () => <PageLayout />);
+storiesOf("Page Layout", module).add("No Header or Body", () => <PageLayout />);
