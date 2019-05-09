36ac0c994d5e79e05648c970ed1a936fd3724d90
“msoyka” <msoyka91@gmail.com>
2019-05-09 11:46:24 -0500

    removed smartavatar

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 import icon from "./avatar.svg";
-import smartAvatar from "./smartAvatar";
+import smartAvatar from "./SmartAvatar";
 
 const Wrapper = styled.figure`
   display: inline-block;
