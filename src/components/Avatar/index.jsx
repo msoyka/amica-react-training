7f3834fa682b01ff2c31777b8a96cae7af80abd4
“msoyka” <msoyka91@gmail.com>
2019-05-09 11:47:00 -0500

    fixed import for smartAvatar

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 import icon from "./avatar.svg";
-import smartAvatar from "./SmartAvatar";
+import smartAvatar from "./smartAvatar";
 
 const Wrapper = styled.figure`
   display: inline-block;
